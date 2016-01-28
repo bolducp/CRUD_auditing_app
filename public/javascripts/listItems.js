d3e75e6d01f93fb856c9bfe6f191c600a77aa5ba
Paige Bolduc <paigebolduc@gmail.com>
2016-01-27 22:29:27 -0800

    update total with search

--- public/javascripts/listItems.js
+++ public/javascripts/listItems.js
@@ -2,15 +2,21 @@
 
 $(document).ready(init);
 
+var $rows = $('.rows')
+
 function init() {
 	$('#search').keyup(searchList);
 }
 
 function searchList() {
-  var $rows = $('.rows')
 	var text = $('#search').val();
 	var $filtered = $rows.filter(function() {
 		return $(this).children('.searchable').text().includes(text);
 	});
+  var sum = 0.00;
+  	$filtered.children('.price').each(function(i, val) {
+  		sum += parseFloat($(val).text().slice(1));
+  	});
+  $('#total').text('Total: $' + sum.toFixed(2));
 	$('tbody').empty().append($filtered);
 }
